36f66d2603c32dd505ff0dd70a67f4310458a0e1
NOTEBOOK-RAMIRO\ramir <ramiro.sovrano@whiteravensoft.com>
2018-07-31 07:20:26 -0300

    .

---
+++ Mobile/SOSApp/SOSApp/SOSApp/ConfiguracionInicial.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Geolocator;
+using System;
+using System.Diagnostics;
+
+namespace SOSApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ConfiguracionInicial : ContentPage
+	{
+		public ConfiguracionInicial ()
+		{
+			InitializeComponent ();
+        }
+
+        public async void test()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(5));
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            
+            test();
+        }
+    }
+}
